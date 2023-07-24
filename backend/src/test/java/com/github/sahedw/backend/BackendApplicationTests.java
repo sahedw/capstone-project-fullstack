4bf78b2a08ca0d9ed65b0649ab942f6c448e8a95
Sahed Wahedi <sahed.w@hotmail.de>
2023-07-24 12:37:24 +0200

    remove empty test

--- backend/src/test/java/com/github/sahedw/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/github/sahedw/backend/BackendApplicationTests.java
@@ -1,13 +1,9 @@
 package com.github.sahedw.backend;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackendApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
