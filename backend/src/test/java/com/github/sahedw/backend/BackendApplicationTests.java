d7f646f285eaca49e9e3dc436ea6c7b4f95adaff
Sahed Wahedi <sahed.w@hotmail.de>
2023-07-24 11:02:06 +0200

    Project Setup

---
+++ backend/src/test/java/com/github/sahedw/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.sahedw.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
