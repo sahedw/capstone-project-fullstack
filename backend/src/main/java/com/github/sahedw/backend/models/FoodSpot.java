46d52c8cabe3a27272007610ceb1b3d47d2052d8
Sahed Wahedi <sahed.w@hotmail.de>
2023-07-25 11:33:37 +0200

    remove last code smell

--- backend/src/main/java/com/github/sahedw/backend/models/FoodSpot.java
+++ backend/src/main/java/com/github/sahedw/backend/models/FoodSpot.java
@@ -3,7 +3,6 @@ package com.github.sahedw.backend.models;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 
