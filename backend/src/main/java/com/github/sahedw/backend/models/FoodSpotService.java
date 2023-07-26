79943d874cbc0b7184912f3fa6803d16eef56312
Sahed Wahedi <sahed.w@hotmail.de>
2023-07-26 12:58:49 +0200

    write service method

--- backend/src/main/java/com/github/sahedw/backend/models/FoodSpotService.java
+++ backend/src/main/java/com/github/sahedw/backend/models/FoodSpotService.java
@@ -4,6 +4,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +20,15 @@ public class FoodSpotService {
     public FoodSpot addFoodSpot(FoodSpot newFoodSpot) {
         foodSpotRepo.insert(newFoodSpot);
         return newFoodSpot;
+    }
+
+    public FoodSpot getById(String id) {
+        Optional<FoodSpot> foundFoodSpot = foodSpotRepo.findById(id);
+        if (foundFoodSpot.isPresent()) {
+            return foundFoodSpot.get();
+        } else {
+            throw new NoSuchElementException();
+        }
     }
 }
 
