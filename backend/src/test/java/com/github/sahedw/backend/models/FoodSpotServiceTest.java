d0ac4e50cd5691a67ff3fa0256e05f888a559d8e
Sahed Wahedi <sahed.w@hotmail.de>
2023-07-26 14:24:12 +0200

    write integration test for service method

--- backend/src/test/java/com/github/sahedw/backend/models/FoodSpotServiceTest.java
+++ backend/src/test/java/com/github/sahedw/backend/models/FoodSpotServiceTest.java
@@ -1,7 +1,9 @@
 package com.github.sahedw.backend.models;
 
 import org.junit.jupiter.api.Test;
+
 import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -16,7 +18,7 @@ class FoodSpotServiceTest {
         //GIVEN
         FoodSpot firstTestFS = new FoodSpot("123", "Sencha Sushi", "Fuhlsbüttler Str. 110", "SUSHI");
         FoodSpot secondTestFS = new FoodSpot("456", "Batman Restaurant", "Steindamm 58", "DOENER");
-        List<FoodSpot> expectedList = List.of(firstTestFS,secondTestFS);
+        List<FoodSpot> expectedList = List.of(firstTestFS, secondTestFS);
         //WHEN
         when(foodSpotRepo.findAll()).thenReturn(expectedList);
         List<FoodSpot> actualList = foodSpotService.allFoodSpots();
@@ -35,5 +37,18 @@ class FoodSpotServiceTest {
         //THEN
         verify(foodSpotRepo).insert(toAddFS);
         assertEquals(toAddFS, actual);
+    }
+
+    @Test
+    void expectSpecificFoodSpot_whenGetByIdIsCalled() {
+        //GIVEN
+        Optional<FoodSpot> expected = Optional.of(
+                new FoodSpot("123", "Sencha Sushi", "Fuhlsbüttler Str. 110", "SUSHI"));
+        String idToFind = "123";
+        //WHEN
+        when(foodSpotRepo.findById(idToFind)).thenReturn(expected);
+        FoodSpot actual = foodSpotService.getById(idToFind);
+        // Assert
+        assertEquals(expected.get(), actual);
     }
 }
