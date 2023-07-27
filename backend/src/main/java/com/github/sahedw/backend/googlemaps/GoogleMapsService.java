0518757c37d5bc67a1b771b227351dee03fc3e0b
Sahed Wahedi <sahed.w@hotmail.de>
2023-07-27 17:21:28 +0200

    adapt method

--- backend/src/main/java/com/github/sahedw/backend/googlemaps/GoogleMapsService.java
+++ backend/src/main/java/com/github/sahedw/backend/googlemaps/GoogleMapsService.java
@@ -1,15 +1,10 @@
 package com.github.sahedw.backend.googlemaps;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.maps.GeoApiContext;
-import com.google.maps.GeocodingApi;
 import com.google.maps.errors.ApiException;
 import com.google.maps.errors.NotFoundException;
 import com.google.maps.model.GeocodingResult;
 import com.google.maps.model.LatLng;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.io.IOException;
@@ -27,7 +22,7 @@ public class GoogleMapsService {
 
     public Position getGeocode(String address) throws IOException, InterruptedException, ApiException {
         GeocodingResult[] results = geocodeApiService.geocode(address);
-        if (results != null && results.length != 0) {
+        if (results != null) {
             LatLng location = results[0].geometry.location;
             return new Position(String.valueOf(location.lat),String.valueOf(location.lng));
         } else {
