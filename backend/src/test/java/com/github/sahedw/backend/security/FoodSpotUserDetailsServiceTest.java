8bad377e295951d41463bcd7fad8f6bb0b2d2020
Sahed Wahedi <sahed.w@hotmail.de>
2023-07-31 17:41:37 +0200

    write new integration tests for user service and controller. Also dapt old test because of security authentication

---
+++ backend/src/test/java/com/github/sahedw/backend/security/FoodSpotUserDetailsServiceTest.java
@@ -0,0 +1,34 @@
+package com.github.sahedw.backend.security;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class FoodSpotUserDetailsServiceTest {
+    FoodSpotUserRepo foodSpotUserRepo = mock(FoodSpotUserRepo.class);
+
+    FoodSpotUserDetailsService foodSpotUserDetailsService = new FoodSpotUserDetailsService(foodSpotUserRepo);
+
+    @Test
+    void loadUserByUsername() {
+        String givenUserName = "sahed";
+        FoodSpotUser expectedUser = new FoodSpotUser("123", "sahed", "sahed1", List.of());
+        when(foodSpotUserRepo.findByUsername(givenUserName)).thenReturn(Optional.of(expectedUser));
+        UserDetails actualUser = foodSpotUserDetailsService.loadUserByUsername(givenUserName);
+        verify(foodSpotUserRepo).findByUsername(givenUserName);
+        Assertions.assertEquals(givenUserName, actualUser.getUsername());
+    }
+
+    @Test
+    void expectUsernameNotFoundException_whenFindByUsername() {
+        Assertions.assertThrows(UsernameNotFoundException.class, () -> foodSpotUserDetailsService.loadUserByUsername("non-existent"));
+    }
+
+}
