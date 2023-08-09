a8fa7ae416516ca8e64b78960d6a48085b8beaa4
Sahed Wahedi <sahed.w@hotmail.de>
2023-08-09 15:30:19 +0200

    navbar with navlink element to highlight actual path

--- frontend/src/components/BurgerMenu.tsx
+++ frontend/src/components/BurgerMenu.tsx
@@ -1,23 +1,29 @@
 import {useState} from 'react';
 import Hamburger from "hamburger-react";
+import {NavLink} from "react-router-dom";
 
-function BurgerMenu(props) {
+function BurgerMenu() {
     const [isOpen, setOpen] = useState<boolean>(false)
     return (<>
             <Hamburger toggled={isOpen}
                        toggle={setOpen}
                        direction={"right"}
                        color={"black"}
-                       label="Show menu"
-                       onToggle={toggled => {
-                           if (toggled) {
-                               console.log("Open")
-                           } else {
-                               console.log("Closed")
-                           }
-                       }}/>
+                       label="Show menu"/>
             <section className={`off-canvas-menu ${isOpen ? "menu-open" : "menu-closed"}`}>
-
+                <nav className={"navbar-container"}>
+                    <ul className={"navbar-list"}>
+                        <li>Add FoodSpots</li>
+                        <NavLink to={"/"}
+                                 className={({ isActive }) =>
+                                     isActive ? "active-link" : ""
+                                 }>
+                            <li>Home</li>
+                        </NavLink>
+                        <li>Map</li>
+                        <li>My Account</li>
+                    </ul>
+                </nav>
             </section>
         </>
 
