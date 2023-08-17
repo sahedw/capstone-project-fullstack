7932b23c2934b4d3430d445d488d0c70f70fb841
Sahed Wahedi <sahed.w@hotmail.de>
2023-08-17 12:25:34 +0200

    small assertion for code smell

--- backend/src/test/java/com/github/sahedw/backend/BackendApplicationTests.java
+++ backend/src/test/java/com/github/sahedw/backend/BackendApplicationTests.java
@@ -1,11 +1,19 @@
 package com.github.sahedw.backend;
 
+import com.github.sahedw.backend.googlemaps.GoogleMapsService;
+import com.github.sahedw.backend.models.FoodSpotService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BackendApplicationTests {
+
+    @Autowired
+    FoodSpotService foodSpotService;
     @Test
     void contextLoads() {
+        Assertions.assertNotNull(foodSpotService);
     }
 }
