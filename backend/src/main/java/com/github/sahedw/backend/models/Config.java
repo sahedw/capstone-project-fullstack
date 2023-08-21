3c83ffd387dcda02d176946a832e59bc7b88a9fa
Sahed Wahedi <sahed.w@hotmail.de>
2023-08-21 13:20:43 +0200

    fix config tests

--- backend/src/main/java/com/github/sahedw/backend/models/Config.java
+++ backend/src/main/java/com/github/sahedw/backend/models/Config.java
@@ -6,20 +6,25 @@ import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
-
 import java.io.IOException;
-
 @Configuration
 public class Config implements WebMvcConfigurer {
+    public static final String DEFAULT_STARTING_PAGE = "static/index.html";
+    static class ReactRoutingPathResourceResolver extends PathResourceResolver {
+        @Override
+        protected Resource getResource(String resourcePath, Resource location) throws IOException {
+            var requestedResource = location.createRelative(resourcePath);
+            if (requestedResource.exists() && requestedResource.isReadable()) {
+                return requestedResource;
+            }
+            return new ClassPathResource(DEFAULT_STARTING_PAGE);
+        }
+    }
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**").addResourceLocations("classpath:/static/").resourceChain(true).addResolver(new PathResourceResolver() {
-            @Override
-            protected Resource getResource(String resourcePath, Resource location) throws IOException {
-                Resource requestedResource = location.createRelative(resourcePath);
-
-                return (requestedResource.exists() && requestedResource.isReadable()) ? requestedResource : new ClassPathResource("/static/index.html");
-            }
-        });
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/static/")
+                .resourceChain(true)
+                .addResolver(new ReactRoutingPathResourceResolver());
     }
 }
--- backend/src/test/java/com/github/sahedw/backend/models/ConfigTest.java
+++ backend/src/test/java/com/github/sahedw/backend/models/ConfigTest.java
@@ -1,27 +1,16 @@
 package com.github.sahedw.backend.models;
 
 import com.github.sahedw.backend.security.SecurityConfig;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
-import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.web.servlet.config.annotation.ResourceChainRegistration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.resource.PathResourceResolver;
-
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 @WebMvcTest({Config.class, SecurityConfig.class})
@@ -29,34 +18,35 @@ import static org.mockito.Mockito.*;
 @AutoConfigureMockMvc
 class ConfigTest {
 
-    @Autowired
-    private MockMvc mockMvc;
-
-    private Config config;
-
-    @Mock
-    private ResourceHandlerRegistry registry = mock(ResourceHandlerRegistry.class);
-
-    @BeforeEach
-    public void setUp() {
-        config = new Config();
+    @Test
+    void expectRelativeResource_ifItExists() throws IOException {
+        // GIVEN
+        var resolver = new Config.ReactRoutingPathResourceResolver();
+        var location = mock(Resource.class);
+        var relativeLocation = mock(Resource.class);
+        var resourcePath = "index.html";
+        when(location.createRelative(resourcePath)).thenReturn(relativeLocation);
+        when(relativeLocation.exists()).thenReturn(true);
+        when(relativeLocation.isReadable()).thenReturn(true);
+        // WHEN
+        var actual = resolver.getResource(resourcePath, location);
+        // THEN
+        Assertions.assertEquals(relativeLocation, actual);
     }
 
     @Test
-    void expectResourceHandlerInRegistry_whenHandlerIsAdded() {
-        //GIVEN
-        ResourceHandlerRegistration registration = mock(ResourceHandlerRegistration.class);
-        ResourceChainRegistration chainRegistration = mock(ResourceChainRegistration.class);
-        //WHEN
-        when(registry.addResourceHandler(any(String.class))).thenReturn(registration);
-        when(registration.addResourceLocations(any(String[].class))).thenReturn(registration);
-        when(registration.resourceChain(any(Boolean.class))).thenReturn(chainRegistration);
-        when(chainRegistration.addResolver(any(PathResourceResolver.class))).thenReturn(chainRegistration);
-        config.addResourceHandlers(registry);
-        //THEN
-        verify(registry).addResourceHandler("/**");
-        verify(registration).addResourceLocations("classpath:/static/");
-        verify(registration).resourceChain(true);
-        verify(chainRegistration).addResolver(any(PathResourceResolver.class));
+    void expectIndexHtml_ifRequestedResourceDoesNotExist() throws IOException {
+        // GIVEN
+        var resolver = new Config.ReactRoutingPathResourceResolver();
+        var location = mock(Resource.class);
+        var relativeLocation = mock(Resource.class);
+        var resourcePath = "index.html";
+        when(location.createRelative(resourcePath)).thenReturn(relativeLocation);
+        when(relativeLocation.exists()).thenReturn(false);
+        // WHEN
+        var actual = resolver.getResource(resourcePath, location);
+        // THEN
+        ClassPathResource expected = new ClassPathResource("static/index.html");
+        Assertions.assertEquals(expected, actual);
     }
 }
